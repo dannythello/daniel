21a021105241632ac792848eeecfbbe0f4cb2579
Akozor Danel <dannythello302@gmail.com>
2023-06-01 11:43:43 +0100

    Inital Commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'header.html';
+?>
+<h1>Welcome</h1>
+<form methods="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])" enctype="multipart/form-data">
+require 'footer.html';
+?>
